41d160506b735d6eec4b4a15d0e42ad77bc8671d
Apisamit <apisamit.seef@bumail.net>
2022-02-25 17:43:12 +0700

    save

---
+++ src/Setting.js
@@ -0,0 +1,13 @@
+import './css/Setting.css';
+
+function Setting() {
+    return (
+        <div>
+            <div className="size">Size</div>
+            <div className="sound">Sound</div>
+            <div className="chat">Chat</div>
+        </div>
+    )
+}
+
+export default Setting;
